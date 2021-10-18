55c505e9aa4e56b177bf1683db0c3ac565867cc9
Bratovan Bratovanov <bratovank@gmail.com>
2021-10-18 13:43:26 +0100

    assignment 1
    widnow alert, variables, concatenate, fontcolor

---
+++ Project1_expressions_alert/main.js
@@ -0,0 +1,8 @@
+
+window.alert("Hello world"); //alert box message
+var a = "Hello"; // first word
+var a = a.fontcolor("red"); //1st word's colour
+var b = " world!"; // 2nd word
+var b = b.fontcolor("blue"); //2nd word's colour
+document.write(a + b); //this displays the variables
+
