f4f3a4841e6808b9a8dd83cd2559e41a1a7297fd
Bratovan Bratovanov <bratovank@gmail.com>
2021-10-18 14:15:47 +0100

    Assignment 2
    +=

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function function1() {
+    var sentence = "I'm learning"; //1st half of the sentence
+    sentence += " a lot from this book."; //2nd half
+    document.getElementById("Concatenate").innerHTML = sentence; //finding an element by id and change the content
+}
