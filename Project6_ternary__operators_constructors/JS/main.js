ecc030e407731549cc640645f1310955a47a96bd
Bratovan Bratovanov <bratovank@gmail.com>
2021-10-21 16:11:57 +0100

    Assignment 6
    nested function, this, new

---
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,24 @@
+//constructor
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function my_Function() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function count_Function() {
+    document.getElementById("counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() { Starting_point += 1;} //nested function adding 1
+        Plus_one();
+        return Starting_point;
+    }
+    
+}
